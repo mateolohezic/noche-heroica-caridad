084693139d1cfdea77b4e27960e03aa8dba67ce4
Mateo Lohezic <mateolohezic@gmail.com>
2023-04-04 12:23:38 -0300

    depl

--- noche-heroica-caridad/src/Pages/Guia/Guia.jsx
+++ noche-heroica-caridad/src/Pages/Guia/Guia.jsx
@@ -4,7 +4,26 @@ import './guia.css'
 function Guia() {
   return (
     <>
-    Guia
+    <div className='w-100 m-0'>
+      <div className='pe-4 pt-5 text-end'>
+        <span className='horariosTitulo text-light'>
+          GUÍA
+        </span>
+      </div>
+      <div className='pt-4 ps-3 pe-3 mt-4 mb-5 text-center'>
+        <div className='oracionInicialTitulo text-light mb-3'><span>Oración Inicial</span></div>
+        <span className='oracionInicialGuia text-light'>
+        Señor Jesús, en esta noche donde te invade el dolor y la angustia, y tu santa frente suda sangre, permítenos acompañarte con nuestra oración y silencio. Queremos permanecer despiertos, orando y acompañándote en tu dolor, meditando tu amor infinito y dando gracias por habernos dejado tu preciosísimo cuerpo, sangre alma y divinidad en el Santísimo Sacramento del Altar. Tú que vives y reinas por los siglos de los siglos.
+        <br/><br/>
+        <b>Amén.</b>
+        </span>
+      </div>
+
+
+
+
+
+    </div>
     </>
   )
 }
